c8ec65d4895360131443b4a12da0cb8cc32e160c
Frankie <frankiejosefej@gmail.com>
2023-05-02 03:36:59 -0400

    met mvp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import SwCharacters from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,9 +10,24 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+  const [characterId, setCharacterId] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then( res => {
+      console.log(res.data)
+      setCharacters(res.data);
+    })
+    .catch( err => {
+      console.error(err);
+    })
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      { characters && <SwCharacters character={characters}/>}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from "react";
+
+ 
+//{name: 'Luke Skywalker', height: '172', mass: '77', hair_color: 'blond', skin_color: 'fair', …}
+
+
+const SwCharacters = (props) => {
+    return (
+        <div className="character-wrapper">
+            {props.character.map((character) => {
+                return(
+                    <div key={character.url}>
+                        <h2>{character.name}</h2>
+                        <p>Born: {character.birth_year}</p>
+                        <p>Gender: {character.gender}</p>
+                        <p>Height: {character.height}</p>
+                        <p>Mass: {character.mass}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default SwCharacters
